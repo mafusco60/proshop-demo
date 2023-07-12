83f74d1ffb720b03263222b9c7b90945a91bab1d
mafusco60 <86462934+mafusco60@users.noreply.github.com>
2023-07-11 23:56:45 -0400

    Sign in Frontend

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -5,14 +5,15 @@ const notFound = (req, res, next) => {
 };
 const errorHandler = (err, req, res, next) => {
 	let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-	res.status(statusCode); // Set the status code to the status code of the response, or 500 if the response status code is 200
+	//res.status(statusCode); // Set the status code to the status code of the response, or 500 if the response status code is 200
 	let message = err.message;
 
-	// Check for Mongoose bad ObjectId
+	//Check for Mongoose bad ObjectId
 	if (err.name === 'CastError' && err.kind === 'ObjectId') {
 		message = 'Resource not found';
 		statusCode = 404;
 	}
+
 	res.status(statusCode).json({
 		message,
 		stack: process.env.NODE_ENV === 'production' ? null : err.stack,
