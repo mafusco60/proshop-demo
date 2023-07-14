a1f57ab812bdaeb4f0ead1def11b460bd684fd01
mafusco60 <86462934+mafusco60@users.noreply.github.com>
2023-07-13 20:53:23 -0400

    Private Route Component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,12 @@
-import { Navbar, Nav, NavDropdown, Container, Badge } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { useLogoutMutation } from '../slices/usersApiSlice';
 import { logout } from '../slices/authSlice';
+import SearchBox from './SearchBox';
 import logo from '../assets/logo.png';
-import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
 	const { cartItems } = useSelector((state) => state.cart);
@@ -22,13 +23,13 @@ const Header = () => {
 			dispatch(logout());
 			navigate('/login');
 		} catch (err) {
-			console.log(err);
+			console.error(err);
 		}
 	};
 
 	return (
 		<header>
-			<Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+			<Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
 				<Container>
 					<LinkContainer to='/'>
 						<Navbar.Brand>
@@ -36,10 +37,10 @@ const Header = () => {
 							ProShop
 						</Navbar.Brand>
 					</LinkContainer>
-
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
 						<Nav className='ms-auto'>
+							<SearchBox />
 							<LinkContainer to='/cart'>
 								<Nav.Link>
 									<FaShoppingCart /> Cart
@@ -51,20 +52,37 @@ const Header = () => {
 								</Nav.Link>
 							</LinkContainer>
 							{userInfo ? (
-								<NavDropdown title={userInfo.name} id='username'>
-									<LinkContainer to='/profile'>
-										<NavDropdown.Item>Profile</NavDropdown.Item>
-									</LinkContainer>
-									<NavDropdown.Item onClick={logoutHandler}>
-										Logout
-									</NavDropdown.Item>
-								</NavDropdown>
+								<>
+									<NavDropdown title={userInfo.name} id='username'>
+										<LinkContainer to='/profile'>
+											<NavDropdown.Item>Profile</NavDropdown.Item>
+										</LinkContainer>
+										<NavDropdown.Item onClick={logoutHandler}>
+											Logout
+										</NavDropdown.Item>
+									</NavDropdown>
+								</>
 							) : (
 								<LinkContainer to='/login'>
 									<Nav.Link>
 										<FaUser /> Sign In
 									</Nav.Link>
 								</LinkContainer>
+							)}
+
+							{/* Admin Links */}
+							{userInfo && userInfo.isAdmin && (
+								<NavDropdown title='Admin' id='adminmenu'>
+									<LinkContainer to='/admin/productlist'>
+										<NavDropdown.Item>Products</NavDropdown.Item>
+									</LinkContainer>
+									<LinkContainer to='/admin/orderlist'>
+										<NavDropdown.Item>Orders</NavDropdown.Item>
+									</LinkContainer>
+									<LinkContainer to='/admin/userlist'>
+										<NavDropdown.Item>Users</NavDropdown.Item>
+									</LinkContainer>
+								</NavDropdown>
 							)}
 						</Nav>
 					</Navbar.Collapse>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,12 +12,15 @@ import store from './store';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App';
+import PrivateRoute from './components/PrivateRoute';
 import reportWebVitals from './reportWebVitals';
 
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
@@ -26,6 +29,11 @@ const router = createBrowserRouter(
 			<Route path='/product/:id' element={<ProductScreen />} />
 			<Route path='/cart' element={<CartScreen />} />
 			<Route path='/login' element={<LoginScreen />} />
+			<Route path='/register' element={<RegisterScreen />} />
+
+			<Route path='' element={<PrivateRoute />}>
+				<Route path='/shipping' element={<ShippingScreen />} />
+			</Route>
 		</Route>
 	)
 );
